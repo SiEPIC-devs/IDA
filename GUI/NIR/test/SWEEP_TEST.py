0d69b6b19f6e8607eb135460277b148e0e0aeeb1
WeijiaKongH <148517177+WeijiaKongH@users.noreply.github.com>
2025-08-18 16:12:06 -0700

    Delete GUI directory

--- GUI/NIR/test/SWEEP_TEST.py
+++
@@ -1,9 +0,0 @@
-# SWEEPYYYY ++++ DATA STITCHING AND COLLECTION>>>>>>
-
-
-#####
-
-#####
-
-
-from NIR.optical_sweeps import *
--- GUI/init_gui.py
+++
@@ -1,5 +0,0 @@
-from lab_gui import *
-
-if __name__ == "__main__":
-    file = File("shared_memory", "", "")
-    file.init()
